a6965810a980c96d5c466fd1813431211578b9fc
Vilsol <me@vil.so>
2021-10-16 21:34:46 +0300

    fix: fix user avatars. fixes #18

--- src/lib/components/mods/ModAuthors.svelte
+++ src/lib/components/mods/ModAuthors.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import type { User, UserMod } from '$lib/generated';
-  import { base } from '$app/paths';
+  import { assets, base } from '$app/paths';
 
   export let authors: Array<
     Pick<UserMod, 'role'> & {
@@ -15,7 +15,9 @@
   <div class="grid auto-rows-min text-lg gap-y-4">
     {#each authors as author}
       <div class="grid grid-flow-col auto-cols-max gap-x-4">
-        <div class="rounded-full bg-cover w-14 h-14" style={`background-image: url("${author.user.avatar}")`} />
+        <div
+          class="rounded-full bg-cover w-14 h-14"
+          style={`background-image: url("${author.user.avatar || assets + '/images/no_image.png'}")`} />
         <div class="grid grid-flow-row">
           <a href="{base}/user/{author.user.id}/" class="text-yellow-500 underline">{author.user.username}</a>
           <div>{author.role.charAt(0).toUpperCase() + author.role.slice(1).toLowerCase()}</div>
--- src/lib/components/users/UserAvatar.svelte
+++ src/lib/components/users/UserAvatar.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
+  import { assets } from '$app/paths';
+
   export let avatar!: string;
   export let username!: string;
+
+  $: userAvatar = avatar || assets + '/images/no_image.png';
 
   // TODO Outdated Flag
 </script>
 
 <div>
   <!-- TODO Fallback to default Logo -->
-  <img class="rounded-lg max-w-lg" src={avatar} alt="Avatar for {username}" />
+  <img class="rounded-lg max-w-lg" src={userAvatar} alt="Avatar for {username}" />
 </div>
