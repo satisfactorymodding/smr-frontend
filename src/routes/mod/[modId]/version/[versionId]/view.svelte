65a030b161d0c7381fb8df2e26c0cc840f9f3a59
Vilsol <me@vil.so>
2022-01-19 22:14:13 +0200

    fix: redirect old view page

---
+++ src/routes/mod/[modId]/version/[versionId]/view.svelte
@@ -0,0 +1,8 @@
+<script context="module">
+  export async function load(input) {
+    return {
+      status: 302,
+      redirect: '/mod/' + input.params.modId + '/version/' + input.params.versionId
+    };
+  }
+</script>
