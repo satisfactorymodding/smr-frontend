50c8520218e99f90a5ce2dcbae937ef6d81fa64b
Vilsol <me@vil.so>
2022-01-20 15:22:33 +0200

    fix: hack around urql-svelte restrictions

--- src/lib/utils/gql.ts
+++ src/lib/utils/gql.ts
@@ -1,34 +1,71 @@
-import type { OperationStore } from '@urql/svelte/dist/types/operationStore';
+import type { OperationStore } from '@urql/svelte';
 import { get, readable } from 'svelte/store';
 import type { Load } from '@sveltejs/kit';
 import { browser } from '$app/env';
 import { gqlClient } from '$lib/stores/global';
 
+// Pretend you didn't see this file
+
 export const loadWaitForNoFetch = (queries: { [key: string]: OperationStore }): Load => {
   return async function () {
+    // If on the browser, initialize queries and start fetching, but instantly return out
     if (browser) {
       const result = Object.keys(queries).map((key) => {
         const query = queries[key];
 
+        // Retrieve the global GQL client
         get(gqlClient)
           .query(query.query, query.variables, query.context)
           .toPromise()
           .then((data) => {
+            // Update query fields without calling set
+            query.update((value) => {
+              (value as unknown)['fetching'] = false;
+              if (data) {
+                (value as unknown)['data'] = data.data;
+                (value as unknown)['error'] = data.error;
+              }
+              return value;
+            });
+
+            // Force query to update its underlying store
             query.set({
-              ...data,
-              fetching: false
+              context: {
+                ...query.context,
+                updated: new Date()
+              }
             });
           })
           .catch((err) => {
+            // Update query fields without calling set
+            query.update((value) => {
+              (value as unknown)['fetching'] = false;
+              (value as unknown)['data'] = undefined;
+              (value as unknown)['error'] = err;
+              return value;
+            });
+
+            // Force query to update its underlying store
             query.set({
-              fetching: false,
-              data: undefined,
-              error: err
+              context: {
+                ...query.context,
+                updated: new Date()
+              }
             });
           });
 
+        // Update query fields without calling set
+        query.update((value) => {
+          (value as unknown)['fetching'] = true;
+          return value;
+        });
+
+        // Force query to update its underlying store
         query.set({
-          fetching: true
+          context: {
+            ...query.context,
+            updated: new Date()
+          }
         });
 
         return [key, query];
@@ -39,6 +76,7 @@ export const loadWaitForNoFetch = (queries: { [key: string]: OperationStore }): 
       };
     }
 
+    // If on SSR, block the load until all queries have returned
     return {
       props: Object.fromEntries(
         await Promise.all(
