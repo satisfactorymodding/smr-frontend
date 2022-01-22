0010f6761df79e165ff2004e0338e30491f8f219
Vilsol <me@vil.so>
2022-01-22 17:43:25 +0200

    chore: cleanup, chunk splitting

--- src/lib/utils/image-cache.ts
+++
@@ -1,6 +0,0 @@
-import { LRUCache } from '../thirdparty/lru_cache';
-
-export const imageCache = new LRUCache({
-  max: 500,
-  maxAge: 1000 * 60
-});
--- svelte.config.js
+++ svelte.config.js
@@ -43,7 +43,47 @@ const config = {
 
     vite: {
       ssr: {
-        noExternal: ['node-fetch', '@urql/svelte', '@cfworker/json-schema', 'dompurify', '@material/animation', '@material/select', '@material/textfield', 'custom-protocol-check']
+        noExternal: [
+          'node-fetch',
+          '@urql/svelte',
+          '@cfworker/json-schema',
+          'dompurify',
+          'custom-protocol-check',
+          '@felte/common',
+          '@material/animation',
+          '@material/base',
+          '@material/button',
+          '@material/card',
+          '@material/checkbox',
+          '@material/circular-progress',
+          '@material/data-table',
+          '@material/density',
+          '@material/dialog',
+          '@material/dom',
+          '@material/drawer',
+          '@material/elevation',
+          '@material/fab',
+          '@material/floating-label',
+          '@material/form-field',
+          '@material/icon-button',
+          '@material/layout-grid',
+          '@material/line-ripple',
+          '@material/list',
+          '@material/menu',
+          '@material/menu-surface',
+          '@material/notched-outline',
+          '@material/ripple',
+          '@material/rtl',
+          '@material/select',
+          '@material/shape',
+          '@material/switch',
+          '@material/textfield',
+          '@material/theme',
+          '@material/tokens',
+          '@material/top-app-bar',
+          '@material/touch-target',
+          '@material/typography'
+        ]
       },
       optimizeDeps: {
         exclude: ['@urql/svelte', 'custom-protocol-check']
@@ -51,7 +91,25 @@ const config = {
       mode: mode,
       build: {
         sourcemap: true,
-        target: ['es2020']
+        target: ['es2020'],
+        rollupOptions: {
+          output: {
+            manualChunks(id) {
+              if (id.includes('jszip')) {
+                return 'jszip';
+              }
+              if (id.includes('zod') || id.includes('felte')) {
+                return 'forms';
+              }
+              if (id.includes('marked') || id.includes('prismjs') || id.includes('dompurify') || id.includes('jsdom')) {
+                return 'extras';
+              }
+              if (id.includes('node_modules')) {
+                return 'vendor';
+              }
+            }
+          }
+        }
       }
     },
 
