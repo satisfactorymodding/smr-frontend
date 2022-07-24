bab8ac1db351a6e17c54adaa4d9f45a06542fb21
Vilsol <me@vil.so>
2022-07-24 18:04:25 +0000

    Deploying to gh-pages from @ satisfactorymodding/smr-frontend@7d142cc2ff94d80ec8a13eaa3d24cba89c20b449 🚀

---
+++ staging/_app/immutable/chunks/stores-84e1d086.js
@@ -0,0 +1,2 @@
+import{D as e}from"./vendor-93f46a94.js";const r=()=>{const s=e("__svelte__");return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},get preloading(){return console.error("stores.preloading is deprecated; use stores.navigating instead"),{subscribe:s.navigating.subscribe}},session:s.session,updated:s.updated}},t={subscribe(s){return r().page.subscribe(s)}};export{t as p};
+//# sourceMappingURL=stores-84e1d086.js.map
