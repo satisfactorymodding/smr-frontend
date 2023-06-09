2829d85594d2ea8384a89af06a43e2fdab9c0e54
budak7273 <computerguy440+gh@gmail.com>
2023-06-09 06:33:18 +0000

    Deploying to gh-pages from @ satisfactorymodding/smr-frontend@ac2eff3e3bd7e7865dd74e2e2bb176cacc6059d8 🚀

---
+++ compatibility-info/service-worker.js
@@ -0,0 +1,55 @@
+const i = { toString: () => {
+  throw new Error("`timestamp` has been removed from $service-worker. Use `version` instead");
+} };
+const t = ["/smr-frontend/compatibility-info/_app/immutable/start-1d443a4e.js", "/smr-frontend/compatibility-info/_app/immutable/pages/__layout.svelte-bb189af8.js", "/smr-frontend/compatibility-info/_app/immutable/assets/pages/__layout.svelte-e327b41b.css", "/smr-frontend/compatibility-info/_app/immutable/error.svelte-d35a6d97.js", "/smr-frontend/compatibility-info/_app/immutable/pages/admin/index.svelte-aae00f17.js", "/smr-frontend/compatibility-info/_app/immutable/assets/pages/admin/index.svelte-dfc5337c.css", "/smr-frontend/compatibility-info/_app/immutable/pages/admin/sml-versions/_smlVersionId_/edit.svelte-d48bf212.js", "/smr-frontend/compatibility-info/_app/immutable/pages/admin/sml-versions/index.svelte-007f751b.js", "/smr-frontend/compatibility-info/_app/immutable/pages/admin/sml-versions/new.svelte-1d89f48f.js", "/smr-frontend/compatibility-info/_app/immutable/pages/admin/tag-manager.svelte-fd11d09a.js", "/smr-frontend/compatibility-info/_app/immutable/pages/admin/unapproved-mods.svelte-85222389.js", "/smr-frontend/compatibility-info/_app/immutable/pages/admin/unapproved-versions.svelte-f06c291a.js", "/smr-frontend/compatibility-info/_app/immutable/pages/api-docs/index.svelte-f66b645a.js", "/smr-frontend/compatibility-info/_app/immutable/assets/pages/api-docs/index.svelte-1c1e4dfd.css", "/smr-frontend/compatibility-info/_app/immutable/pages/guide/_guideId_/edit.svelte-8c1bbb05.js", "/smr-frontend/compatibility-info/_app/immutable/pages/guide/_guideId_/index.svelte-f12dd2d9.js", "/smr-frontend/compatibility-info/_app/immutable/pages/guides/index.svelte-ca87e38a.js", "/smr-frontend/compatibility-info/_app/immutable/pages/help/index.svelte-ecb63ef4.js", "/smr-frontend/compatibility-info/_app/immutable/pages/index.svelte-f2f63aa3.js", "/smr-frontend/compatibility-info/_app/immutable/assets/pages/index.svelte-fe20af75.css", "/smr-frontend/compatibility-info/_app/immutable/pages/mod/_modId_/edit.svelte-57a00c6c.js", "/smr-frontend/compatibility-info/_app/immutable/pages/mod/_modId_/index.svelte-dfdd85d4.js", "/smr-frontend/compatibility-info/_app/immutable/assets/pages/mod/_modId_/index.svelte-1e2ec0d7.css", "/smr-frontend/compatibility-info/_app/immutable/pages/mod/_modId_/new-version.svelte-f2bd54c5.js", "/smr-frontend/compatibility-info/_app/immutable/pages/mod/_modId_/version/_versionId_/edit.svelte-73c2bdb4.js", "/smr-frontend/compatibility-info/_app/immutable/pages/mod/_modId_/version/_versionId_/index.svelte-d7d685ac.js", "/smr-frontend/compatibility-info/_app/immutable/pages/mod/_modId_/version/_versionId_/view.svelte-9faedf57.js", "/smr-frontend/compatibility-info/_app/immutable/pages/mods/index.svelte-7e9093af.js", "/smr-frontend/compatibility-info/_app/immutable/assets/pages/mods/index.svelte-07b34bd3.css", "/smr-frontend/compatibility-info/_app/immutable/pages/new-guide/index.svelte-bac81cb8.js", "/smr-frontend/compatibility-info/_app/immutable/pages/new-mod/index.svelte-c6354747.js", "/smr-frontend/compatibility-info/_app/immutable/pages/privacy-policy/index.svelte-3c0eab11.js", "/smr-frontend/compatibility-info/_app/immutable/pages/settings/index.svelte-ade0c1ad.js", "/smr-frontend/compatibility-info/_app/immutable/pages/sml-versions/index.svelte-f3d36521.js", "/smr-frontend/compatibility-info/_app/immutable/pages/tools/index.svelte-8ee65f02.js", "/smr-frontend/compatibility-info/_app/immutable/pages/tos/index.svelte-9c3dab43.js", "/smr-frontend/compatibility-info/_app/immutable/pages/user/_userId_/index.svelte-53f1ef4f.js", "/smr-frontend/compatibility-info/_app/immutable/chunks/extras-8984d02f.js", "/smr-frontend/compatibility-info/_app/immutable/chunks/forms-a09581a5.js", "/smr-frontend/compatibility-info/_app/immutable/chunks/jszip-0721729d.js", "/smr-frontend/compatibility-info/_app/immutable/chunks/vendor-5faff0be.js", "/smr-frontend/compatibility-info/_app/immutable/chunks/paths-1c47712a.js", "/smr-frontend/compatibility-info/_app/immutable/chunks/singletons-cdeec3fd.js", "/smr-frontend/compatibility-info/_app/immutable/chunks/preload-helper-e4440c9d.js", "/smr-frontend/compatibility-info/_app/immutable/chunks/global-fb89089b.js", "/smr-frontend/compatibility-info/_app/immutable/chunks/user-cee14147.js", "/smr-frontend/compatibility-info/_app/immutable/chunks/navigation-ce539261.js", "/smr-frontend/compatibility-info/_app/immutable/chunks/launcher-aafc45b7.js", "/smr-frontend/compatibility-info/_app/immutable/chunks/api-bb6d5972.js", "/smr-frontend/compatibility-info/_app/immutable/chunks/graphql-39b507e4.js", "/smr-frontend/compatibility-info/_app/immutable/chunks/stores-a6f790a9.js", "/smr-frontend/compatibility-info/_app/immutable/chunks/Toast-13eea52f.js", "/smr-frontend/compatibility-info/_app/immutable/chunks/MetaDescriptors-5e0585a5.js", "/smr-frontend/compatibility-info/_app/immutable/chunks/routing-c6322149.js", "/smr-frontend/compatibility-info/_app/immutable/chunks/SMLVersionForm-ba209b96.js", "/smr-frontend/compatibility-info/_app/immutable/assets/SMLVersionForm-1cc6d1a4.css", "/smr-frontend/compatibility-info/_app/immutable/chunks/forms-694c548d.js", "/smr-frontend/compatibility-info/_app/immutable/chunks/markdown-6a2ff7f0.js", "/smr-frontend/compatibility-info/_app/immutable/chunks/PageControls-3bf45888.js", "/smr-frontend/compatibility-info/_app/immutable/chunks/formatting-d1753ec3.js", "/smr-frontend/compatibility-info/_app/immutable/chunks/GuideForm-a3c2486b.js", "/smr-frontend/compatibility-info/_app/immutable/chunks/gql-24499839.js", "/smr-frontend/compatibility-info/_app/immutable/chunks/GuideCard-4391f7c1.js", "/smr-frontend/compatibility-info/_app/immutable/chunks/FicsitCard-c800fd7d.js", "/smr-frontend/compatibility-info/_app/immutable/assets/FicsitCard-4de49b41.css", "/smr-frontend/compatibility-info/_app/immutable/chunks/uplugin-ba5939c1.js", "/smr-frontend/compatibility-info/_app/immutable/chunks/ModCard-796e4a05.js", "/smr-frontend/compatibility-info/_app/immutable/chunks/CompatibilityButton-c23833ac.js", "/smr-frontend/compatibility-info/_app/immutable/chunks/TagList-f09044fd.js", "/smr-frontend/compatibility-info/_app/immutable/assets/TagList-92a9d7da.css", "/smr-frontend/compatibility-info/_app/immutable/chunks/ModForm-5978db4c.js", "/smr-frontend/compatibility-info/_app/immutable/chunks/ModCompatibilityEdit-ff23b44f.js", "/smr-frontend/compatibility-info/_app/immutable/chunks/VersionForm-6618f253.js", "/smr-frontend/compatibility-info/_app/immutable/chunks/custom_protocol-9a0f337a.js"];
+const e = ["/smr-frontend/compatibility-info/assets/android-chrome-192x192.png", "/smr-frontend/compatibility-info/assets/android-chrome-512x512.png", "/smr-frontend/compatibility-info/assets/apple-touch-icon.png", "/smr-frontend/compatibility-info/assets/browserconfig.xml", "/smr-frontend/compatibility-info/assets/favicon-16x16.png", "/smr-frontend/compatibility-info/assets/favicon-32x32.png", "/smr-frontend/compatibility-info/assets/favicon.ico", "/smr-frontend/compatibility-info/assets/fonts/flow-rounded.woff", "/smr-frontend/compatibility-info/assets/mstile-150x150.png", "/smr-frontend/compatibility-info/assets/safari-pinned-tab.svg", "/smr-frontend/compatibility-info/images/Satisfactory_KeyArt_1_1024x1024_NoLogo.webp", "/smr-frontend/compatibility-info/images/bg_nologo.webp", "/smr-frontend/compatibility-info/images/dog_aw_yis_pats.gif", "/smr-frontend/compatibility-info/images/dog_boing.gif", "/smr-frontend/compatibility-info/images/dog_stand.gif", "/smr-frontend/compatibility-info/images/mods_mobile.webp", "/smr-frontend/compatibility-info/images/no_image.webp", "/smr-frontend/compatibility-info/images/satisfactory_logo_full_color_small.webp", "/smr-frontend/compatibility-info/images/sf_modding_logo.webp", "/smr-frontend/compatibility-info/images/smm_background.webp", "/smr-frontend/compatibility-info/images/smm_hand.webp", "/smr-frontend/compatibility-info/images/smm_icon_white.webp", "/smr-frontend/compatibility-info/manifest.json", "/smr-frontend/compatibility-info/robots.txt", "/smr-frontend/compatibility-info/smui.css"];
+const s = self;
+const n = `cache${i}`;
+const a = t.concat(e);
+const o = new Set(a);
+s.addEventListener("install", (i2) => {
+  i2.waitUntil(caches.open(n).then((i3) => i3.addAll(a)).then(() => {
+    s.skipWaiting();
+  }));
+});
+s.addEventListener("activate", (i2) => {
+  i2.waitUntil(caches.keys().then(async (i3) => {
+    for (const t2 of i3) {
+      if (t2 !== n) {
+        await caches.delete(t2);
+      }
+    }
+    s.clients.claim();
+  }));
+});
+async function m(t2) {
+  const e2 = await caches.open(`offline${i}`);
+  try {
+    const i2 = await fetch(t2);
+    e2.put(t2, i2.clone());
+    return i2;
+  } catch (i2) {
+    const s2 = await e2.match(t2);
+    if (s2) {
+      return s2;
+    }
+    throw i2;
+  }
+}
+s.addEventListener("fetch", (i2) => {
+  if (i2.request.method !== "GET" || i2.request.headers.has("range")) {
+    return;
+  }
+  const t2 = new URL(i2.request.url);
+  const e2 = t2.protocol.startsWith("http");
+  const s2 = t2.hostname === self.location.hostname && t2.port !== self.location.port;
+  const n2 = t2.host === self.location.host && o.has(t2.pathname);
+  const a2 = i2.request.cache === "only-if-cached" && !n2;
+  if (e2 && !s2 && !a2 && n2 && !i2.request.url.includes("start")) {
+    i2.respondWith((async () => {
+      const t3 = n2 && await caches.match(i2.request);
+      return t3 || m(i2.request);
+    })());
+  }
+});
+//# sourceMappingURL=service-worker.js.map
