31ff8a61c1a7ea9071cb3bbe4770e2f21a4836a8
Chris <porisius@gmail.com>
2023-06-21 16:02:49 -0400

    urlWrap for source and Tag movement/formatting

--- src/lib/components/mods/ModInfo.svelte
+++ src/lib/components/mods/ModInfo.svelte
@@ -10,20 +10,28 @@
     Mod,
     'source_url' | 'created_at' | 'views' | 'downloads' | 'mod_reference' | 'compatibility' | 'tags'
   >;
+
+  function urlWrap(url: string) {
+    if (url.length > 40) {
+      return url.replace(/\/(?!.*\/)/g, '/\n');
+    }
+    return url;
+  }
 </script>
 
 <Card>
   <Content>
     <div class="text-lg break-words">
       <h3 class="text-2xl my-4 font-bold">Info</h3>
-      <span
-        ><strong>Source:</strong> <a class="underline text-yellow-500" href={mod.source_url}>{mod.source_url}</a></span
+      <span><strong>Tags::</strong><TagList tags={mod.tags} /></span>
+      <span style="white-space: pre-line"
+        ><strong>Source:</strong>
+        <a class="underline text-yellow-500" href={mod.source_url}>{urlWrap(mod.source_url)}</a></span
       ><br />
       <span><strong>Created:</strong> {prettyDate(mod.created_at)}</span><br />
       <span><strong>Views:</strong> {prettyNumber(mod.views)}</span><br />
       <span><strong>Downloads:</strong> {prettyNumber(mod.downloads)}</span><br />
       <span><strong>Reference:</strong> {mod.mod_reference}</span><br />
-      <TagList tags={mod.tags} />
       {#if mod.compatibility}
         <span><strong>Compatibility:</strong><CompatibilityButton compatibility={mod.compatibility} /> </span><br />
       {/if}
