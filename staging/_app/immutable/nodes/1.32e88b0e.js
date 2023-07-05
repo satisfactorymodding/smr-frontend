25604cddc85042c0787085601655efed31675876
Vilsol <me@vil.so>
2023-07-05 05:11:02 +0000

    Deploying to gh-pages from @ satisfactorymodding/smr-frontend@7dbe5f54f62586929ad909cdd96a1616079f67ab 🚀

---
+++ staging/_app/immutable/nodes/1.32e88b0e.js
@@ -0,0 +1,2 @@
+import{bc as e}from"../chunks/vendor.2dadc702.js";export{e as component};
+//# sourceMappingURL=1.32e88b0e.js.map
--- staging/service-worker.js
+++ staging/service-worker.js
@@ -1,55 +1,139 @@
-const s = { toString: () => {
-  throw new Error("`timestamp` has been removed from $service-worker. Use `version` instead");
-} };
-const e = ["/smr-frontend/staging/_app/immutable/start-3d33787b.js", "/smr-frontend/staging/_app/immutable/pages/__layout.svelte-14c9fb62.js", "/smr-frontend/staging/_app/immutable/assets/pages/__layout.svelte-e327b41b.css", "/smr-frontend/staging/_app/immutable/error.svelte-d35a6d97.js", "/smr-frontend/staging/_app/immutable/pages/admin/index.svelte-aae00f17.js", "/smr-frontend/staging/_app/immutable/assets/pages/admin/index.svelte-dfc5337c.css", "/smr-frontend/staging/_app/immutable/pages/admin/sml-versions/_smlVersionId_/edit.svelte-d48bf212.js", "/smr-frontend/staging/_app/immutable/pages/admin/sml-versions/index.svelte-007f751b.js", "/smr-frontend/staging/_app/immutable/pages/admin/sml-versions/new.svelte-1d89f48f.js", "/smr-frontend/staging/_app/immutable/pages/admin/tag-manager.svelte-fd11d09a.js", "/smr-frontend/staging/_app/immutable/pages/admin/unapproved-mods.svelte-85222389.js", "/smr-frontend/staging/_app/immutable/pages/admin/unapproved-versions.svelte-f06c291a.js", "/smr-frontend/staging/_app/immutable/pages/api-docs/index.svelte-f66b645a.js", "/smr-frontend/staging/_app/immutable/assets/pages/privacy-policy/index.svelte-bcc48566.css", "/smr-frontend/staging/_app/immutable/pages/guide/_guideId_/edit.svelte-8c1bbb05.js", "/smr-frontend/staging/_app/immutable/pages/guide/_guideId_/index.svelte-f12dd2d9.js", "/smr-frontend/staging/_app/immutable/pages/guides/index.svelte-ca87e38a.js", "/smr-frontend/staging/_app/immutable/pages/help/index.svelte-ecb63ef4.js", "/smr-frontend/staging/_app/immutable/pages/index.svelte-b23d3055.js", "/smr-frontend/staging/_app/immutable/assets/pages/index.svelte-fe20af75.css", "/smr-frontend/staging/_app/immutable/pages/mod/_modId_/edit.svelte-57a00c6c.js", "/smr-frontend/staging/_app/immutable/pages/mod/_modId_/index.svelte-55c8bcd0.js", "/smr-frontend/staging/_app/immutable/assets/pages/mod/_modId_/index.svelte-1e2ec0d7.css", "/smr-frontend/staging/_app/immutable/pages/mod/_modId_/new-version.svelte-f2bd54c5.js", "/smr-frontend/staging/_app/immutable/pages/mod/_modId_/version/_versionId_/edit.svelte-73c2bdb4.js", "/smr-frontend/staging/_app/immutable/pages/mod/_modId_/version/_versionId_/index.svelte-d7d685ac.js", "/smr-frontend/staging/_app/immutable/pages/mod/_modId_/version/_versionId_/view.svelte-9faedf57.js", "/smr-frontend/staging/_app/immutable/pages/mods/index.svelte-5ea7fc44.js", "/smr-frontend/staging/_app/immutable/assets/pages/mods/index.svelte-07b34bd3.css", "/smr-frontend/staging/_app/immutable/pages/new-guide/index.svelte-bac81cb8.js", "/smr-frontend/staging/_app/immutable/pages/new-mod/index.svelte-c6354747.js", "/smr-frontend/staging/_app/immutable/pages/privacy-policy/index.svelte-3c0eab11.js", "/smr-frontend/staging/_app/immutable/pages/settings/index.svelte-ade0c1ad.js", "/smr-frontend/staging/_app/immutable/pages/sml-versions/index.svelte-f3d36521.js", "/smr-frontend/staging/_app/immutable/pages/tools/index.svelte-8ee65f02.js", "/smr-frontend/staging/_app/immutable/pages/tos/index.svelte-9c3dab43.js", "/smr-frontend/staging/_app/immutable/pages/user/_userId_/index.svelte-ac857b31.js", "/smr-frontend/staging/_app/immutable/chunks/extras-8984d02f.js", "/smr-frontend/staging/_app/immutable/chunks/forms-a09581a5.js", "/smr-frontend/staging/_app/immutable/chunks/jszip-0721729d.js", "/smr-frontend/staging/_app/immutable/chunks/vendor-5faff0be.js", "/smr-frontend/staging/_app/immutable/chunks/paths-1c47712a.js", "/smr-frontend/staging/_app/immutable/chunks/singletons-cdeec3fd.js", "/smr-frontend/staging/_app/immutable/chunks/preload-helper-b2fcde9f.js", "/smr-frontend/staging/_app/immutable/chunks/global-fb89089b.js", "/smr-frontend/staging/_app/immutable/chunks/user-cee14147.js", "/smr-frontend/staging/_app/immutable/chunks/navigation-ce539261.js", "/smr-frontend/staging/_app/immutable/chunks/launcher-aafc45b7.js", "/smr-frontend/staging/_app/immutable/chunks/api-bb6d5972.js", "/smr-frontend/staging/_app/immutable/chunks/graphql-39b507e4.js", "/smr-frontend/staging/_app/immutable/chunks/stores-a6f790a9.js", "/smr-frontend/staging/_app/immutable/chunks/Toast-13eea52f.js", "/smr-frontend/staging/_app/immutable/chunks/MetaDescriptors-5e0585a5.js", "/smr-frontend/staging/_app/immutable/chunks/routing-c6322149.js", "/smr-frontend/staging/_app/immutable/chunks/SMLVersionForm-ba209b96.js", "/smr-frontend/staging/_app/immutable/assets/ModForm-cc1db5c2.css", "/smr-frontend/staging/_app/immutable/chunks/forms-694c548d.js", "/smr-frontend/staging/_app/immutable/chunks/markdown-6a2ff7f0.js", "/smr-frontend/staging/_app/immutable/chunks/PageControls-3bf45888.js", "/smr-frontend/staging/_app/immutable/chunks/formatting-d1753ec3.js", "/smr-frontend/staging/_app/immutable/chunks/GuideForm-a3c2486b.js", "/smr-frontend/staging/_app/immutable/chunks/gql-24499839.js", "/smr-frontend/staging/_app/immutable/chunks/GuideCard-4391f7c1.js", "/smr-frontend/staging/_app/immutable/chunks/FicsitCard-c800fd7d.js", "/smr-frontend/staging/_app/immutable/assets/FicsitCard-4de49b41.css", "/smr-frontend/staging/_app/immutable/chunks/uplugin-ba5939c1.js", "/smr-frontend/staging/_app/immutable/chunks/ModCard-c07a2e7d.js", "/smr-frontend/staging/_app/immutable/chunks/CompatibilityButton-593d5045.js", "/smr-frontend/staging/_app/immutable/chunks/TagList-f09044fd.js", "/smr-frontend/staging/_app/immutable/assets/TagList-92a9d7da.css", "/smr-frontend/staging/_app/immutable/chunks/ModForm-5978db4c.js", "/smr-frontend/staging/_app/immutable/chunks/ModCompatibilityEdit-ff23b44f.js", "/smr-frontend/staging/_app/immutable/chunks/VersionForm-6618f253.js", "/smr-frontend/staging/_app/immutable/chunks/custom_protocol-9a0f337a.js"];
-const n = ["/smr-frontend/staging/assets/android-chrome-192x192.png", "/smr-frontend/staging/assets/android-chrome-512x512.png", "/smr-frontend/staging/assets/apple-touch-icon.png", "/smr-frontend/staging/assets/browserconfig.xml", "/smr-frontend/staging/assets/favicon-16x16.png", "/smr-frontend/staging/assets/favicon-32x32.png", "/smr-frontend/staging/assets/favicon.ico", "/smr-frontend/staging/assets/fonts/flow-rounded.woff", "/smr-frontend/staging/assets/mstile-150x150.png", "/smr-frontend/staging/assets/safari-pinned-tab.svg", "/smr-frontend/staging/images/Satisfactory_KeyArt_1_1024x1024_NoLogo.webp", "/smr-frontend/staging/images/bg_nologo.webp", "/smr-frontend/staging/images/dog_aw_yis_pats.gif", "/smr-frontend/staging/images/dog_boing.gif", "/smr-frontend/staging/images/dog_stand.gif", "/smr-frontend/staging/images/mods_mobile.webp", "/smr-frontend/staging/images/no_image.webp", "/smr-frontend/staging/images/satisfactory_logo_full_color_small.webp", "/smr-frontend/staging/images/sf_modding_logo.webp", "/smr-frontend/staging/images/smm_background.webp", "/smr-frontend/staging/images/smm_hand.webp", "/smr-frontend/staging/images/smm_icon_white.webp", "/smr-frontend/staging/manifest.json", "/smr-frontend/staging/robots.txt", "/smr-frontend/staging/smui.css"];
-const t = self;
-const a = `cache${s}`;
-const i = e.concat(n);
-const m = new Set(i);
-t.addEventListener("install", (s2) => {
-  s2.waitUntil(caches.open(a).then((s3) => s3.addAll(i)).then(() => {
-    t.skipWaiting();
-  }));
+const s = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), b = [
+  s + "/_app/immutable/entry/app.eb170a52.js",
+  s + "/_app/immutable/assets/0.abc8fe6d.css",
+  s + "/_app/immutable/nodes/0.200526e2.js",
+  s + "/_app/immutable/nodes/1.32e88b0e.js",
+  s + "/_app/immutable/assets/10.6b0d351c.css",
+  s + "/_app/immutable/nodes/10.8c4afc6b.js",
+  s + "/_app/immutable/nodes/11.bd27c65f.js",
+  s + "/_app/immutable/nodes/12.69a0697c.js",
+  s + "/_app/immutable/nodes/13.0af833bf.js",
+  s + "/_app/immutable/nodes/14.1ccd77a1.js",
+  s + "/_app/immutable/assets/15.eb1d8953.css",
+  s + "/_app/immutable/nodes/15.451e442b.js",
+  s + "/_app/immutable/nodes/16.c5002fcc.js",
+  s + "/_app/immutable/nodes/17.6c879ac4.js",
+  s + "/_app/immutable/nodes/18.64ff5cc0.js",
+  s + "/_app/immutable/nodes/19.a9143a0c.js",
+  s + "/_app/immutable/assets/2.7e9d1551.css",
+  s + "/_app/immutable/nodes/2.f33e9af0.js",
+  s + "/_app/immutable/nodes/20.32d2c8e0.js",
+  s + "/_app/immutable/assets/21.9e076a89.css",
+  s + "/_app/immutable/nodes/21.7366b94b.js",
+  s + "/_app/immutable/nodes/22.16f4a41c.js",
+  s + "/_app/immutable/nodes/23.46822df3.js",
+  s + "/_app/immutable/assets/24.bd2edfc1.css",
+  s + "/_app/immutable/nodes/24.af3ad4e6.js",
+  s + "/_app/immutable/nodes/25.c6dcae4d.js",
+  s + "/_app/immutable/nodes/26.2e237139.js",
+  s + "/_app/immutable/nodes/27.6a72ff98.js",
+  s + "/_app/immutable/assets/28.0796b3f1.css",
+  s + "/_app/immutable/nodes/28.789d02ca.js",
+  s + "/_app/immutable/nodes/29.20f6c504.js",
+  s + "/_app/immutable/assets/3.de29dae2.css",
+  s + "/_app/immutable/nodes/3.b9949f86.js",
+  s + "/_app/immutable/nodes/4.57bfd78b.js",
+  s + "/_app/immutable/nodes/5.dcafe13e.js",
+  s + "/_app/immutable/nodes/6.925d239d.js",
+  s + "/_app/immutable/nodes/7.beb4a825.js",
+  s + "/_app/immutable/nodes/8.3d3b2576.js",
+  s + "/_app/immutable/nodes/9.5957078c.js",
+  s + "/_app/immutable/assets/FicsitCard.be5c19d7.css",
+  s + "/_app/immutable/assets/GuideForm.f869d2a9.css",
+  s + "/_app/immutable/assets/ModForm.d6437a6e.css",
+  s + "/_app/immutable/assets/SMLVersionForm.cffc0340.css",
+  s + "/_app/immutable/assets/TagList.c4cc72d7.css",
+  s + "/_app/immutable/assets/VersionForm.8deea9e4.css",
+  s + "/_app/immutable/chunks/CompatibilityButton.3c556142.js",
+  s + "/_app/immutable/chunks/FicsitCard.afe7812f.js",
+  s + "/_app/immutable/chunks/GuideCard.8ce1768e.js",
+  s + "/_app/immutable/chunks/GuideForm.fbc9d8c6.js",
+  s + "/_app/immutable/chunks/MetaDescriptors.c839152c.js",
+  s + "/_app/immutable/chunks/ModCard.5bbfdeb7.js",
+  s + "/_app/immutable/chunks/ModCompatibilityEdit.af1d27df.js",
+  s + "/_app/immutable/chunks/ModForm.1e9dd2b5.js",
+  s + "/_app/immutable/chunks/PageControls.a2fde333.js",
+  s + "/_app/immutable/chunks/SMLVersionForm.537aa733.js",
+  s + "/_app/immutable/chunks/TagList.8bf4a7cb.js",
+  s + "/_app/immutable/chunks/Toast.3d7ae842.js",
+  s + "/_app/immutable/chunks/VersionForm.1c8be474.js",
+  s + "/_app/immutable/chunks/api.736caaaa.js",
+  s + "/_app/immutable/chunks/extras.dc97897a.js",
+  s + "/_app/immutable/chunks/formatting.6f84ef8f.js",
+  s + "/_app/immutable/chunks/forms.032aefcd.js",
+  s + "/_app/immutable/chunks/forms.30c79ce1.js",
+  s + "/_app/immutable/chunks/global.e51727cb.js",
+  s + "/_app/immutable/chunks/gql.ff9241aa.js",
+  s + "/_app/immutable/chunks/graphql.769656ab.js",
+  s + "/_app/immutable/chunks/jszip.3769576e.js",
+  s + "/_app/immutable/chunks/launcher.663350f9.js",
+  s + "/_app/immutable/chunks/markdown.623802d0.js",
+  s + "/_app/immutable/chunks/preload-helper.12267524.js",
+  s + "/_app/immutable/chunks/uplugin.dc05efb2.js",
+  s + "/_app/immutable/chunks/user.cdc98260.js",
+  s + "/_app/immutable/chunks/vendor.2dadc702.js",
+  s + "/_app/immutable/entry/start.4be81b8e.js",
+  s + "/_app/immutable/chunks/custom_protocol.0ef1eb93.js"
+], l = [
+  s + "/assets/android-chrome-192x192.png",
+  s + "/assets/android-chrome-512x512.png",
+  s + "/assets/apple-touch-icon.png",
+  s + "/assets/browserconfig.xml",
+  s + "/assets/favicon-16x16.png",
+  s + "/assets/favicon-32x32.png",
+  s + "/assets/favicon.ico",
+  s + "/assets/fonts/flow-rounded.woff",
+  s + "/assets/mstile-150x150.png",
+  s + "/assets/safari-pinned-tab.svg",
+  s + "/images/Satisfactory_KeyArt_1_1024x1024_NoLogo.webp",
+  s + "/images/bg_nologo.webp",
+  s + "/images/dog_aw_yis_pats.gif",
+  s + "/images/dog_boing.gif",
+  s + "/images/dog_stand.gif",
+  s + "/images/mods_mobile.webp",
+  s + "/images/no_image.webp",
+  s + "/images/satisfactory_logo_full_color_small.webp",
+  s + "/images/sf_modding_logo.webp",
+  s + "/images/smm_background.webp",
+  s + "/images/smm_hand.webp",
+  s + "/images/smm_icon_white.webp",
+  s + "/manifest.json",
+  s + "/robots.txt",
+  s + "/smui.css"
+], i = "1688533806556", m = self, o = `cache${i}`, n = b.concat(l), d = new Set(n);
+m.addEventListener("install", (a) => {
+  a.waitUntil(
+    caches.open(o).then((e) => e.addAll(n)).then(() => {
+      m.skipWaiting();
+    })
+  );
 });
-t.addEventListener("activate", (s2) => {
-  s2.waitUntil(caches.keys().then(async (s3) => {
-    for (const e2 of s3) {
-      if (e2 !== a) {
-        await caches.delete(e2);
-      }
-    }
-    t.clients.claim();
-  }));
+m.addEventListener("activate", (a) => {
+  a.waitUntil(
+    caches.keys().then(async (e) => {
+      for (const t of e)
+        t !== o && await caches.delete(t);
+      m.clients.claim();
+    })
+  );
 });
-async function r(e2) {
-  const n2 = await caches.open(`offline${s}`);
+async function _(a) {
+  const e = await caches.open(`offline${i}`);
   try {
-    const s2 = await fetch(e2);
-    n2.put(e2, s2.clone());
-    return s2;
-  } catch (s2) {
-    const t2 = await n2.match(e2);
-    if (t2) {
-      return t2;
-    }
-    throw s2;
+    const t = await fetch(a);
+    return e.put(a, t.clone()), t;
+  } catch (t) {
+    const p = await e.match(a);
+    if (p)
+      return p;
+    throw t;
   }
 }
-t.addEventListener("fetch", (s2) => {
-  if (s2.request.method !== "GET" || s2.request.headers.has("range")) {
+m.addEventListener("fetch", (a) => {
+  if (a.request.method !== "GET" || a.request.headers.has("range"))
     return;
-  }
-  const e2 = new URL(s2.request.url);
-  const n2 = e2.protocol.startsWith("http");
-  const t2 = e2.hostname === self.location.hostname && e2.port !== self.location.port;
-  const a2 = e2.host === self.location.host && m.has(e2.pathname);
-  const i2 = s2.request.cache === "only-if-cached" && !a2;
-  if (n2 && !t2 && !i2 && a2 && !s2.request.url.includes("start")) {
-    s2.respondWith((async () => {
-      const e3 = a2 && await caches.match(s2.request);
-      return e3 || r(s2.request);
-    })());
-  }
+  const e = new URL(a.request.url), t = e.protocol.startsWith("http"), p = e.hostname === self.location.hostname && e.port !== self.location.port, c = e.host === self.location.host && d.has(e.pathname), u = a.request.cache === "only-if-cached" && !c;
+  t && !p && !u && c && !a.request.url.includes("start") && a.respondWith(
+    (async () => c && await caches.match(a.request) || _(a.request))()
+  );
 });
-//# sourceMappingURL=service-worker.js.map
