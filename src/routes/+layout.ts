b76b44f1faa7b58ee46620106172f25677240482
Vilsol <me@vil.so>
2023-09-16 17:37:32 +0300

    chore: fix run scripts, enable prerender

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,6 +1,8 @@
 import type { LayoutLoad } from './$types';
 import { initializeGraphQLClient } from '$lib/core';
 
+export const prerender = 'auto';
+
 export const load: LayoutLoad = async ({ fetch }) => {
   const client = initializeGraphQLClient(fetch);
   return {
