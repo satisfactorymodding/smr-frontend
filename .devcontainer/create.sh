25eabe5924acc3420aeb1d365074164e9e2d1db3
Rob B <computerguy440+gh@gmail.com>
2023-09-17 13:23:04 -0400

    chore: devcontainer: git safe folder

--- .devcontainer/create.sh
+++ .devcontainer/create.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Add as a safe git directory
+git config --global --add safe.directory "/workspaces/smr_frontend"
+
 # Install watchman
 sudo apt-get update && sudo apt-get install -y watchman
 
