3d339be7eebd12781c919b29910633c610790f0a
budak7273 <computerguy440+gh@gmail.com>
2023-12-18 08:17:45 +0000

    Deploying to gh-pages from @ satisfactorymodding/smr-frontend@e77bb6ba559cc23c5ad3ca2d77128404e5cd27b2 🚀

---
+++ hidden-indicator/_app/immutable/entry/start.ed0840b9.js
@@ -0,0 +1,2 @@
+import{b7 as a}from"../chunks/vendor.4474e4ea.js";export{a as start};
+//# sourceMappingURL=start.ed0840b9.js.map
---
+++ hidden-indicator/service-worker.js
@@ -0,0 +1,139 @@
+const s = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), d = [
+  s + "/_app/immutable/entry/app.f13e1404.js",
+  s + "/_app/immutable/assets/0.3df520dc.css",
+  s + "/_app/immutable/nodes/0.c29db72d.js",
+  s + "/_app/immutable/nodes/1.f212c191.js",
+  s + "/_app/immutable/assets/10.6b0d351c.css",
+  s + "/_app/immutable/nodes/10.78752a3c.js",
+  s + "/_app/immutable/nodes/11.48df8129.js",
+  s + "/_app/immutable/nodes/12.523b6ac3.js",
+  s + "/_app/immutable/nodes/13.f10f883b.js",
+  s + "/_app/immutable/nodes/14.ce5c2c83.js",
+  s + "/_app/immutable/assets/15.eb1d8953.css",
+  s + "/_app/immutable/nodes/15.5d6af247.js",
+  s + "/_app/immutable/nodes/16.2a0fd1ad.js",
+  s + "/_app/immutable/nodes/17.f4d8b3de.js",
+  s + "/_app/immutable/nodes/18.db4859bb.js",
+  s + "/_app/immutable/nodes/19.71171390.js",
+  s + "/_app/immutable/assets/2.7e9d1551.css",
+  s + "/_app/immutable/nodes/2.559b2121.js",
+  s + "/_app/immutable/nodes/20.c0417c4f.js",
+  s + "/_app/immutable/assets/21.9e076a89.css",
+  s + "/_app/immutable/nodes/21.e5f17a3c.js",
+  s + "/_app/immutable/nodes/22.576fa603.js",
+  s + "/_app/immutable/nodes/23.a6335cd5.js",
+  s + "/_app/immutable/assets/24.bd2edfc1.css",
+  s + "/_app/immutable/nodes/24.56ddd7f7.js",
+  s + "/_app/immutable/nodes/25.01adc63c.js",
+  s + "/_app/immutable/nodes/26.6a06c403.js",
+  s + "/_app/immutable/nodes/27.82aefd65.js",
+  s + "/_app/immutable/assets/28.0796b3f1.css",
+  s + "/_app/immutable/nodes/28.9c2c17f8.js",
+  s + "/_app/immutable/nodes/29.74b4edd9.js",
+  s + "/_app/immutable/assets/3.de29dae2.css",
+  s + "/_app/immutable/nodes/3.6ad79d6e.js",
+  s + "/_app/immutable/nodes/4.5e0f0431.js",
+  s + "/_app/immutable/nodes/5.9e5efcbe.js",
+  s + "/_app/immutable/nodes/6.8d53cdd4.js",
+  s + "/_app/immutable/nodes/7.11612294.js",
+  s + "/_app/immutable/nodes/8.f88b0885.js",
+  s + "/_app/immutable/nodes/9.efce2836.js",
+  s + "/_app/immutable/assets/AnnouncementRow.29fd166c.css",
+  s + "/_app/immutable/assets/FicsitCard.e2ca203a.css",
+  s + "/_app/immutable/assets/GuideForm.f869d2a9.css",
+  s + "/_app/immutable/assets/ModForm.d6437a6e.css",
+  s + "/_app/immutable/assets/SMLVersionForm.cffc0340.css",
+  s + "/_app/immutable/assets/VersionForm.6f46e1d6.css",
+  s + "/_app/immutable/chunks/AnnouncementRow.b7a1c977.js",
+  s + "/_app/immutable/chunks/CompatibilityModal.62774677.js",
+  s + "/_app/immutable/chunks/FicsitCard.7da80b16.js",
+  s + "/_app/immutable/chunks/GuideCard.b773f0ea.js",
+  s + "/_app/immutable/chunks/GuideForm.585f8e03.js",
+  s + "/_app/immutable/chunks/MetaDescriptors.001e0789.js",
+  s + "/_app/immutable/chunks/ModCard.5cb72b08.js",
+  s + "/_app/immutable/chunks/ModCompatibilityEdit.724ea016.js",
+  s + "/_app/immutable/chunks/ModForm.a63c5be9.js",
+  s + "/_app/immutable/chunks/Page404.ec394d38.js",
+  s + "/_app/immutable/chunks/SMLVersionForm.ea51b019.js",
+  s + "/_app/immutable/chunks/TagList.898f2e85.js",
+  s + "/_app/immutable/chunks/VersionDependenciesGrid.597425ec.js",
+  s + "/_app/immutable/chunks/VersionForm.713ef38c.js",
+  s + "/_app/immutable/chunks/api.853e29e3.js",
+  s + "/_app/immutable/chunks/extras.c2d6af18.js",
+  s + "/_app/immutable/chunks/formatting.28d70897.js",
+  s + "/_app/immutable/chunks/forms.3e9d48d2.js",
+  s + "/_app/immutable/chunks/forms.c5a340bb.js",
+  s + "/_app/immutable/chunks/gql.ff9241aa.js",
+  s + "/_app/immutable/chunks/graphql.e6600caa.js",
+  s + "/_app/immutable/chunks/jszip.42dbc4d1.js",
+  s + "/_app/immutable/chunks/launcher.dcacdd57.js",
+  s + "/_app/immutable/chunks/markdown.c72bdd07.js",
+  s + "/_app/immutable/chunks/preload-helper.8b93628a.js",
+  s + "/_app/immutable/chunks/uplugin.e13bbc6e.js",
+  s + "/_app/immutable/chunks/user.c825de90.js",
+  s + "/_app/immutable/chunks/vendor.4474e4ea.js",
+  s + "/_app/immutable/assets/vendor.bd53154f.css",
+  s + "/_app/immutable/entry/start.ed0840b9.js",
+  s + "/_app/immutable/chunks/custom_protocol.0ef1eb93.js"
+], b = [
+  s + "/assets/android-chrome-192x192.png",
+  s + "/assets/android-chrome-512x512.png",
+  s + "/assets/apple-touch-icon.png",
+  s + "/assets/browserconfig.xml",
+  s + "/assets/favicon-16x16.png",
+  s + "/assets/favicon-32x32.png",
+  s + "/assets/favicon.ico",
+  s + "/assets/fonts/flow-rounded.woff",
+  s + "/assets/mstile-150x150.png",
+  s + "/assets/safari-pinned-tab.svg",
+  s + "/images/Satisfactory_KeyArt_1_1024x1024_NoLogo.webp",
+  s + "/images/bg_nologo.webp",
+  s + "/images/dog_aw_yis_pats.gif",
+  s + "/images/dog_boing.gif",
+  s + "/images/dog_stand.gif",
+  s + "/images/mods_mobile.webp",
+  s + "/images/no_image.webp",
+  s + "/images/satisfactory_logo_full_color_small.webp",
+  s + "/images/sf_modding_logo.webp",
+  s + "/images/smm_background.webp",
+  s + "/images/smm_hand.webp",
+  s + "/images/smm_icon_white.webp",
+  s + "/manifest.json",
+  s + "/robots.txt"
+], c = "1702887390996", m = self, n = `cache${c}`, o = d.concat(b), l = new Set(o);
+m.addEventListener("install", (a) => {
+  a.waitUntil(
+    caches.open(n).then((e) => e.addAll(o)).then(() => {
+      m.skipWaiting();
+    })
+  );
+});
+m.addEventListener("activate", (a) => {
+  a.waitUntil(
+    caches.keys().then(async (e) => {
+      for (const t of e)
+        t !== n && await caches.delete(t);
+      m.clients.claim();
+    })
+  );
+});
+async function _(a) {
+  const e = await caches.open(`offline${c}`);
+  try {
+    const t = await fetch(a);
+    return e.put(a, t.clone()), t;
+  } catch (t) {
+    const p = await e.match(a);
+    if (p)
+      return p;
+    throw t;
+  }
+}
+m.addEventListener("fetch", (a) => {
+  if (a.request.method !== "GET" || a.request.headers.has("range"))
+    return;
+  const e = new URL(a.request.url), t = e.protocol.startsWith("http"), p = e.hostname === self.location.hostname && e.port !== self.location.port, i = e.host === self.location.host && l.has(e.pathname), u = a.request.cache === "only-if-cached" && !i;
+  t && !p && !u && i && !a.request.url.includes("start") && a.respondWith(
+    (async () => i && await caches.match(a.request) || _(a.request))()
+  );
+});
