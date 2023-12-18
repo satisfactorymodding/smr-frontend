514f05002d7db5b9a0c24059a2f53343bc6fbae3
Rob B <computerguy440+gh@gmail.com>
2023-12-18 08:28:26 +0000

    feat: display warning on unapproved versions

--- src/lib/components/versions/VersionDescription.svelte
+++ src/lib/components/versions/VersionDescription.svelte
@@ -1,16 +1,28 @@
 <script lang="ts">
+  import { AnnouncementImportance } from '$lib/generated';
   import { markdown } from '$lib/utils/markdown';
+  import AnnouncementRow from '../announcements/AnnouncementRow.svelte';
 
   export let changelog!: string;
+  export let approved: boolean;
 </script>
 
-<div class="h-fit card p-4">
-  <section>
-    <div class="markdown-content">
-      {#await markdown(changelog) then changelogRendered}
-        <!-- eslint-disable -->
-        <p>{@html changelogRendered}</p>
-      {/await}
+<div>
+  {#if !approved}
+    <div class="p-4">
+      <AnnouncementRow
+        importance={AnnouncementImportance.Warning}
+        message="This version has not yet been approved by the virus scanner and is hidden until this process is complete. If this message isn't gone in the next few minutes, ask for manual approval on the Discord." />
     </div>
-  </section>
+  {/if}
+  <div class="h-fit card p-4">
+    <section>
+      <div class="markdown-content break-words">
+        {#await markdown(changelog) then changelogRendered}
+          <!-- eslint-disable -->
+          <p>{@html changelogRendered}</p>
+        {/await}
+      </div>
+    </section>
+  </div>
 </div>
