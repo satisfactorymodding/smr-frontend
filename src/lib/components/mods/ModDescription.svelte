e77bb6ba559cc23c5ad3ca2d77128404e5cd27b2
Rob B <computerguy440+gh@gmail.com>
2023-12-18 08:13:43 +0000

    feat: visual indicator for if a mod is hidden (closes #130)

--- src/lib/components/mods/ModDescription.svelte
+++ src/lib/components/mods/ModDescription.svelte
@@ -1,19 +1,29 @@
 <script lang="ts">
-  import type { Mod } from '$lib/generated';
+  import { AnnouncementImportance, type Mod } from '$lib/generated';
   import { markdown } from '$lib/utils/markdown';
+  import AnnouncementRow from '../announcements/AnnouncementRow.svelte';
 
-  export let mod!: Pick<Mod, 'full_description' | 'short_description'>;
+  export let mod!: Pick<Mod, 'full_description' | 'short_description' | 'hidden'>;
 
   $: description = mod.full_description ? markdown(mod.full_description) : mod.short_description;
 </script>
 
-<div class="h-fit card p-4">
-  <section>
-    <div class="markdown-content break-words">
-      {#await description then descriptionRendered}
-        <!-- eslint-disable -->
-        <p>{@html descriptionRendered}</p>
-      {/await}
+<div>
+  {#if mod.hidden}
+    <div class="p-4">
+      <AnnouncementRow
+        importance={AnnouncementImportance.Info}
+        message="This mod is hidden, which means it won't display in searches, and the Mod Manager will not display any view/download count or logo for it. A developer could chose to hide their mod for any number of reasons. Perhaps the mod is a library mod and not intended to be viewed directly, or maybe the developer doesn't want to develop the mod any more. Check the mod page for more information." />
     </div>
-  </section>
+  {/if}
+  <div class="h-fit card p-4">
+    <section>
+      <div class="markdown-content break-words">
+        {#await description then descriptionRendered}
+          <!-- eslint-disable -->
+          <p>{@html descriptionRendered}</p>
+        {/await}
+      </div>
+    </section>
+  </div>
 </div>
