45d19b6bfc86b00cb2e22eacc5956182dde2ad96
Rob B <computerguy440+gh@gmail.com>
2024-01-06 08:48:35 +0000

    fix: add spacing at the bottom of the tags collapse section to avoid paginator drawing over tooltips (z index isn't helping for some reason)

--- src/lib/components/utils/TagDisplay.svelte
+++ src/lib/components/utils/TagDisplay.svelte
@@ -19,6 +19,7 @@
 </div>
 
 <!-- Must use high z-index to ensure it draws on top of things like striped compatibility overlays in mod cards -->
+<!-- For some reason this still isn't able to draw over the ModGrid paginator-->
 <!-- https://floating-ui.com/docs/misc#z-index-stacking -->
 <div class="card variant-filled-surface p-4" data-popup={`popupHoverFor${tag.name}`} style="z-index: 1000">
   <p>{tag.description}.</p>
