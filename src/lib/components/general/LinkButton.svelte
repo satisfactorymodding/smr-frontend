e10fce58fd3cfd49a900bc10dd085d32f707471c
Rob B <computerguy440+gh@gmail.com>
2024-10-05 20:17:25 +0000

    feat: link button component, unban form link, layout work

---
+++ src/lib/components/general/LinkButton.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let url: string;
+  export let icon: string;
+  export let extraCss = '';
+</script>
+
+<a href={url} target="_blank" rel="noopener" class={`variant-ghost-primary btn btn-sm ${extraCss}`}>
+  <span><slot>Unset button text!</slot></span>
+  <span class="material-icons">{icon}</span>
+</a>
--- src/routes/community/+page.svelte
+++ src/routes/community/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import { goto } from '$app/navigation';
+  import LinkButton from '$lib/components/general/LinkButton.svelte';
   import MetaDescriptors from '$lib/components/utils/MetaDescriptors.svelte';
+  import { T } from '@tolgee/svelte';
 </script>
 
 <svelte:head>
@@ -13,27 +15,29 @@
 <div>
   <div class="grid grid-flow-col grid-cols-1 grid-rows-2 gap-4 md:grid-cols-2 md:grid-rows-1">
     <div class="card p-4">
-      <div class="grid grid-flow-col grid-cols-1 grid-rows-2 gap-4 2xl:grid-cols-2 2xl:grid-rows-1">
+      <div
+        class="grid grid-flow-col grid-cols-1 grid-rows-2 justify-items-center gap-4 2xl:grid-cols-2 2xl:grid-rows-1">
         <div>
-          <h2>Discord</h2>
+          <h2><T keyName="community.discord.header" defaultValue="Modding Community Discord" /></h2>
           <p>
-            The Satisfactory Modding Discord server is the primary place for modding discussion and support. Click the
-            button below to join the server.
+            <T
+              keyName="community.discord.description"
+              defaultValue="The Satisfactory Modding Discord server is the primary place for modding discussion and support. Click the
+            button below to join the server." />
           </p>
-          <button class="variant-ghost-primary btn btn-sm" on:click={() => goto('https://discord.ficsit.app/')}>
-            <span>Join the Discord</span>
-            <span class="material-icons">people</span>
-          </button>
-          <h2>Ban Appeals</h2>
+          <LinkButton url="https://discord.ficsit.app/" icon="people">
+            <T keyName="community.discord.join-button" defaultValue="Join the Discord" />
+          </LinkButton>
+          <h2><T keyName="community.discord-bans.header" defaultValue="Ban Appeals" /></h2>
           <p>
-            If you have been banned from the Discord server and would like to appeal your ban, you can do so using the
-            form linked below.
+            <T
+              keyName="community.discord-bans.description"
+              defaultValue="If you have been banned from the Discord server and would like to appeal your ban, you can do so using the
+            form linked below." />
           </p>
-          <!-- TODO actual unban forum -->
-          <button class="variant-ghost-primary btn btn-sm" on:click={() => goto('https://discord.ficsit.app/')}>
-            <span>Submit an Appeal</span>
-            <span class="material-icons">how_to_vote</span>
-          </button>
+          <LinkButton url="https://dyno.gg/form/b3425ef2" icon="how_to_vote">
+            <T keyName="community.discord-bans.appeal-button" defaultValue="Submit an Appeal" />
+          </LinkButton>
         </div>
         <iframe
           src="https://discord.com/widget?id=555424930502541343&theme=dark"
@@ -59,6 +63,12 @@
           <span>Visit the Welcome Guide</span>
           <span class="material-icons">waving_hand</span>
         </button>
+        <LinkButton
+          url="https://docs.ficsit.app/satisfactory-modding/latest/ForUsers/Welcome.html"
+          icon="waving_hand"
+          extraCss="text-2xl">
+          <T keyName="community.welcome.guide-button" defaultValue="Visit the Welcome Guide" />
+        </LinkButton>
         <h2>Archived Forums</h2>
         <p>
           The community used to have a Discourse-powered forums website for modding discussion, but it has since been
@@ -68,6 +78,9 @@
           <span>Open Archived Forums</span>
           <span class="material-icons">forum</span>
         </button>
+        <LinkButton url="https://forums.ficsit.app/" icon="forum">
+          <T keyName="community.forums.visit-button" defaultValue="View the Archived Forums" />
+        </LinkButton>
       </section>
     </div>
   </div>
