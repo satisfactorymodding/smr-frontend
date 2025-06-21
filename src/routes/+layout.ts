8eae7a36e1e0002d991442705a33c07ccd1671c0
Rob B <computerguy440+gh@gmail.com>
2025-06-21 12:34:42 -0400

    fix: some translations still displaying english data (#216)

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -4,13 +4,18 @@ import { DevTools, FormatSimple, LanguageDetector, Tolgee } from '@tolgee/svelte
 import { browser } from '$app/environment';
 import { PUBLIC_TOLGEE_API_KEY, PUBLIC_TOLGEE_API_URL } from '$env/static/public';
 
-import deCommon from '../i18n/common/de.json';
 import enCommon from '../i18n/common/en.json';
-import frCommon from '../i18n/common/fr.json';
+import deCommon from '../i18n/common/de.json';
+import esCommon from '../i18n/common/es.json';
+import huCommon from '../i18n/common/hu.json';
 import itCommon from '../i18n/common/it.json';
+import koCommon from '../i18n/common/ko.json';
+import frCommon from '../i18n/common/fr.json';
 import lvCommon from '../i18n/common/lv.json';
 import mtCommon from '../i18n/common/mt.json';
 import nlCommon from '../i18n/common/nl.json';
+import ptBrCommon from '../i18n/common/pt-BR.json';
+import plCommon from '../i18n/common/pl.json';
 import ruCommon from '../i18n/common/ru.json';
 import zhHansCommon from '../i18n/common/zh-Hans.json';
 import zhHantCommon from '../i18n/common/zh-Hant.json';
@@ -35,14 +40,20 @@ export const load: LayoutLoad = async ({ fetch }) => {
     apiUrl: browser ? PUBLIC_TOLGEE_API_URL : undefined,
     apiKey: browser ? PUBLIC_TOLGEE_API_KEY : undefined,
 
+    // Be sure to also update TranslationDropdown.svelte when adding entries here
     staticData: {
-      'de:common': deCommon,
       'en:common': enCommon,
+      'de:common': deCommon,
+      'es:common': esCommon,
       'fr:common': frCommon,
+      'hu:common': huCommon,
       'it:common': itCommon,
+      'ko:common': koCommon,
       'lv:common': lvCommon,
       'mt:common': mtCommon,
       'nl:common': nlCommon,
+      'pt-BR:common': ptBrCommon,
+      'pl:common': plCommon,
       'ru:common': ruCommon,
       'zh-Hans:common': zhHansCommon,
       'zh-Hant:common': zhHantCommon
