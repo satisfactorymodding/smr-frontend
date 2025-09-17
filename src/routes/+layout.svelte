592c8aa3c4e8a9d938cf3fa948bb91c6cb40590b
Vilsol <me@vil.so>
2025-09-18 00:25:51 +0300

    chore: rename app.css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,7 +14,7 @@
   import { TolgeeProvider } from '@tolgee/svelte';
   import { initializeStores, Modal, storePopup, Drawer, Toast } from '@skeletonlabs/skeleton';
   import TopBar from '$lib/components/general/TopBar.svelte';
-  import '../app.pcss';
+  import '../app.css';
   import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
 
   interface Props {
