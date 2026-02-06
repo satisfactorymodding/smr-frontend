6c382925f7b97890adc23340a43d0c0f9b882224
rhit-mooretj <112582846+rhit-mooretj@users.noreply.github.com>
2026-02-06 16:31:19 -0500

    Some more modpacks implementation

--- src/lib/components/modpacks/ModpackCard.svelte
+++ src/lib/components/modpacks/ModpackCard.svelte
@@ -2,7 +2,7 @@
   import type { Modpack, Maybe, Version } from '$lib/generated';
   import { base } from '$app/paths';
   import FicsitCard from '$lib/components/general/FicsitCard.svelte';
-//   import { installMod } from '$lib/stores/launcher';
+  import { installModpack } from '$lib/stores/launcher';
   import { prettyNumber } from '$lib/utils/formatting';
 //   import OutdatedBanner from '$lib/components/mods/compatibility/OutdatedBanner.svelte';
 //   import CompatibilityButton from '$lib/components/mods/compatibility/CompatibilityButton.svelte';
@@ -35,13 +35,11 @@
     <TagList tags={modpack.tags} />
   </div>
   <svelte:fragment slot="actions">
-    <!-- {#if installable}
-      <button class="variant-soft-surface btn btn-sm" title="Install" on:click={() => installMod(mod.mod_reference)}>
-        <span class="material-icons">download</span>
-      </button>
-    {/if} -->
+    <button class="variant-soft-surface btn btn-sm" title="Install" on:click={() => installModpack(modpack.id)}>
+    <span class="material-icons">download</span>
+    </button>
   </svelte:fragment>
   <!-- <div slot="outer">
-    <OutdatedBanner compatibility={mod.compatibility} />
+    <OutdatedBanner compatibility={modpack.compatibility} />
   </div> -->
 </FicsitCard>
