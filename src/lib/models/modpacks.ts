39ebc4859f59d864a917f2e54b6058302c293424
rhit-mooretj <112582846+rhit-mooretj@users.noreply.github.com>
2026-02-06 07:32:17 +0000

    made progress

--- src/lib/models/modpacks.ts
+++ src/lib/models/modpacks.ts
@@ -1,70 +1,31 @@
 import * as zod from 'zod'; 
-import type { CompatibilityState, Tag } from '$lib/generated';
 
-// Currently copied from mods.ts until I implement.
-
-export type ModData = {
+export type ModpackData = {
   name: string;
-  mod_reference: string;
   short_description: string;
   full_description?: string;
   logo?: string;
-  source_url?: string;
-  authors?: {
-    role: string;
-    user_id: string;
-  }[];
-  compatibility?: {
-    EA: {
-      state: CompatibilityState;
-      note?: string;
-    };
-    EXP: {
-      state: CompatibilityState;
-      note?: string;
-    };
-  };
+  logo_thumbhash?: string; // Unsure if this is a string, type not shown in txt
+  creator_id: string;
+  views: number;
+  hotness: number;
+  installs: number;
+  popularity: number;
+  readonly parent_id?: string; // Readonly as it should not be changed after creation
   hidden: boolean;
-  tagIDs?: string[];
-  tags?: Tag[];
 };
 
-export const modSchema = zod.object({
+export const modpackSchema = zod.object({
   name: zod.string().min(3).max(32),
-  mod_reference: zod
-    .string()
-    .min(3)
-    .max(32)
-    .regex(/^([a-zA-Z][a-zA-Z0-9_]*)$/)
-    .refine(
-      async () =>
-        // TODO Check if mod reference exists
-        true
-    ),
   short_description: zod.string().min(16).max(128),
-  full_description: zod.optional(zod.string()),
+  full_description: zod.string(),
   logo: zod.optional(zod.any().refine((logo) => 'name' in logo && 'size' in logo && 'type' in logo)),
-  source_url: zod.string().url().optional().or(zod.literal('')),
-  authors: zod.optional(
-    zod
-      .object({
-        role: zod.string(),
-        user_id: zod.string()
-      })
-      .array()
-  ),
-  compatibility: zod.optional(
-    zod.object({
-      EA: zod.object({
-        state: zod.string(),
-        note: zod.ostring()
-      }),
-      EXP: zod.object({
-        state: zod.string(),
-        note: zod.ostring()
-      })
-    })
-  ),
-  hidden: zod.boolean(),
-  tagIDs: zod.optional(zod.string().array())
+  logo_thumbhash: zod.string().optional(),
+  creator_id: zod.string().uuid(),
+  views: zod.number().min(0).default(0),
+  hotness: zod.number().min(0).default(0),
+  installs: zod.number().min(0).default(0),
+  popularity: zod.number().min(0).default(0),
+  parent_id: zod.string().uuid().optional().readonly(),
+  hidden: zod.boolean().default(false)
 });
