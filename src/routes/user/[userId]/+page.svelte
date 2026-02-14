92ca9d4f0a4bf0dffc2ab8ac01f28bde90bccf0a
rhit-mooretj <112582846+rhit-mooretj@users.noreply.github.com>
2026-02-14 03:05:51 -0500

    Refactored user page to allow for tabs and showing modpacks

--- src/routes/user/[userId]/+page.svelte
+++ src/routes/user/[userId]/+page.svelte
@@ -8,12 +8,16 @@
   import { base } from '$app/paths';
   import type { PageData } from './$types';
   import Page404 from '$lib/components/general/Page404.svelte';
+    import { Tab, TabGroup } from '@skeletonlabs/skeleton';
+    import { getTranslate } from '@tolgee/svelte';
 
   export let data: PageData;
 
+  export const { t } = getTranslate();
+
   $: ({ user } = data);
 
-  let guidesTab = false;
+  let tabSet = 'mods';
 </script>
 
 <svelte:head>
@@ -38,59 +42,60 @@
           <a class="variant-ghost-primary btn" href="{base}/settings">Settings</a>
         {/if}
 
-        <TabGroup class="w-full">
-          <div class="flex w-full border-b border-surface-300">
-            <div class="flex-1 text-center">
-              <Tab bind:group={tabSet} name="info" value="info">
-                Modpack Info
-              </Tab>
-            </div>
-            <div class="flex-1 text-center">
-              <Tab bind:group={tabSet} name="list" value="list">
-                Mod List
-              </Tab>
-            </div>
+        <div class="grid gap-8 xl:grid-cols-4 mt-4">
+          <div class="xl:col-span-3">
+            <TabGroup class="w-full">
+              <div class="flex w-full border-b border-surface-300">
+                <div class="flex-1 text-center">
+                  <Tab bind:group={tabSet} name="mods" value="mods">
+                    {$t('sidebar.mods')}
+                  </Tab>
+                </div>
+                <div class="flex-1 text-center">
+                  <Tab bind:group={tabSet} name="modpacks" value="modpacks">
+                    {$t('sidebar.modpacks')}
+                  </Tab>
+                </div>
+                <div class="flex-1 text-center">
+                  <Tab bind:group={tabSet} name="guides" value="guides">
+                    {$t('sidebar.guides')}
+                  </Tab>
+                </div>
+              </div>
+
+              <svelte:fragment slot="panel">
+                {#if tabSet === 'mods'}
+                  <div class="grid h-fit grid-cols-1 gap-4 2xl:grid-cols-2 3xl:grid-cols-3">
+                    {#each $user.data.getUser.mods as mod}
+                      <ModCard mod={mod.mod} />
+                    {/each}
+                    {#if $user.data.getUser.mods.length === 0}
+                      User has no mods
+                    {/if}
+                  </div>
+                {:else if tabSet === 'modpacks'}
+                  TODO
+                {:else if tabSet === 'guides'}
+                  <div class="grid h-fit grid-cols-1 gap-4 2xl:grid-cols-2 3xl:grid-cols-3">
+                    {#each $user.data.getUser.guides as guide}
+                      <GuideCard {guide} logo={$user.data.getUser.avatar} />
+                    {/each}
+                    {#if $user.data.getUser.guides.length === 0}
+                      User has no guides
+                    {/if}
+                  </div>
+                {/if}
+              </svelte:fragment>
+            </TabGroup>
           </div>
 
-
-
-        <button class="variant-ghost-primary btn" on:click={() => (guidesTab = !guidesTab)}>
-
-          {#if !guidesTab}
-            Guides
-          {:else}
-            Mods
-          {/if}
-        </button>
-      </div>
-    </div>
-    <div class="grid-auto-max grid auto-cols-fr gap-4">
-      {#if !guidesTab}
-        <div class="grid h-fit grid-cols-1 gap-4 2xl:grid-cols-2 3xl:grid-cols-3">
-          {#each $user.data.getUser.mods as mod}
-            <ModCard mod={mod.mod} />
-          {/each}
-
-          {#if $user.data.getUser.mods.length === 0}
-            <!-- TODO Prettier -->
-            User has no mods
-          {/if}
+          <aside class="xl:col-span-1">
+            <div class="grid auto-rows-min gap-8">
+              <UserAvatar avatar={$user.data.getUser.avatar} username={$user.data.getUser.username} />
+              <UserInfo user={$user.data.getUser} />
+            </div>
+          </aside>
         </div>
-      {:else}
-        <div class="grid h-fit grid-cols-1 gap-4 2xl:grid-cols-2 3xl:grid-cols-3">
-          {#each $user.data.getUser.guides as guide}
-            <GuideCard {guide} logo={$user.data.getUser.avatar} />
-          {/each}
-
-          {#if $user.data.getUser.guides.length === 0}
-            <!-- TODO Prettier -->
-            User has no guides
-          {/if}
-        </div>
-      {/if}
-      <div class="grid auto-rows-min grid-cols-1 gap-8">
-        <UserAvatar avatar={$user.data.getUser.avatar} username={$user.data.getUser.username} />
-        <UserInfo user={$user.data.getUser} />
       </div>
     </div>
   </div>
