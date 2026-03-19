87f3dac59a664dd10524e053fd0d754f726660d5
rhit-mooretj <112582846+rhit-mooretj@users.noreply.github.com>
2026-03-19 02:00:17 -0400

    Compatability progress

---
+++ src/lib/components/modpacks/compatibility/CompatibilityButton.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import type { CompatibilityInfoInput } from '$lib/generated';
+  import CompatibilityIcon from './CompatibilityIcon.svelte';
+  import { getTranslate } from '@tolgee/svelte';
+  import { getModalStore } from '@skeletonlabs/skeleton';
+  import CompatibilityModal from '$lib/modals/CompatibilityModal.svelte';
+
+  export let compatibility: CompatibilityInfoInput;
+
+  export const { t } = getTranslate();
+
+  const modalStore = getModalStore();
+  const openCompatibility = () => {
+    modalStore.trigger({
+      type: 'component',
+      component: {
+        ref: CompatibilityModal,
+        props: {
+          compatibility
+        }
+      }
+    });
+  };
+</script>
+
+<button
+  class="variant-soft-surface btn m-0 min-w-0 px-1 py-0 text-xs"
+  title={$t('compatibility-info.button')}
+  on:click={openCompatibility}>
+  <CompatibilityIcon compatibility={compatibility?.EA} />
+  <CompatibilityIcon compatibility={compatibility?.EXP} EXP={true} />
+</button>
---
+++ src/lib/components/modpacks/compatibility/CompatibilityIcon.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import type { CompatibilityInput } from '$lib/generated';
+  import { CompatibilityState } from '$lib/generated';
+
+  export let compatibility: CompatibilityInput = null;
+  export let EXP = false;
+  let iconText = 'rocket_launch';
+  if (EXP) {
+    iconText = 'science';
+  }
+</script>
+
+<p
+  class="material-icons text-sm"
+  class:modpack-state-works={compatibility?.state === CompatibilityState.Works}
+  class:modpack-state-damaged={compatibility?.state === CompatibilityState.Damaged}
+  class:modpack-state-broken={compatibility?.state === CompatibilityState.Broken}
+  class:modpack-state-unknown={!compatibility}>
+  {iconText}
+</p>
