73f20e419489f74f38e6db4159fd35a0fbb9b59e
rhit-mooretj <112582846+rhit-mooretj@users.noreply.github.com>
2026-04-04 23:43:15 -0500

    feat: modpack compatibility is now calculated based on modpack specific compatibility and worst case of mods included

--- src/lib/models/modpacks.ts
+++ src/lib/models/modpacks.ts
@@ -1,3 +1,4 @@
+import type { CompatibilityState } from '$lib/generated/graphql';
 import * as zod from 'zod';
 
 export type ModpackData = {
@@ -7,6 +8,16 @@ export type ModpackData = {
   logo?: string;
   logo_thumbhash?: string; // Unsure if this is a string, type not shown in txt
   creator_id: string;
+  compatibility?: {
+      EA: {
+        state: CompatibilityState;
+        note?: string;
+      };
+      EXP: {
+        state: CompatibilityState;
+        note?: string;
+      };
+    };
   views: number;
   hotness: number;
   installs: number;
@@ -22,6 +33,18 @@ export const modpackSchema = zod.object({
   logo: zod.optional(zod.any().refine((logo) => 'name' in logo && 'size' in logo && 'type' in logo)),
   logo_thumbhash: zod.string().optional(),
   creator_id: zod.string().uuid(),
+  compatibility: zod.optional(
+      zod.object({
+        EA: zod.object({
+          state: zod.string(),
+          note: zod.ostring()
+        }),
+        EXP: zod.object({
+          state: zod.string(),
+          note: zod.ostring()
+        })
+      })
+    ),
   views: zod.number().min(0).default(0),
   hotness: zod.number().min(0).default(0),
   installs: zod.number().min(0).default(0),
