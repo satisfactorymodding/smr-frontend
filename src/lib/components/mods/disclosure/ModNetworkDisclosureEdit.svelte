56a8011944dc28db75acbc623ce53b5ac89833a2
Rob B <computerguy440+gh@gmail.com>
2026-04-08 03:07:55 -0400

    fix: make Network Disclosure dropdown options translatable, mark description required when presented

--- src/lib/components/mods/disclosure/ModNetworkDisclosureEdit.svelte
+++ src/lib/components/mods/disclosure/ModNetworkDisclosureEdit.svelte
@@ -2,23 +2,32 @@
   import { valueForUnspecifiedDisclosure, valueForNoUsageDisclosure } from '$lib/utils/mod';
   import { getTolgee, getTranslate, T } from '@tolgee/svelte';
 
+  // Since network usage disclosure is stored as a null/empty string/non-empty string,
+  // the enum of states only exists here on the frontend
   // eslint bug? https://stackoverflow.com/questions/63961803/eslint-says-all-enums-in-typescript-app-are-already-declared-in-the-upper-scope
   // eslint-disable-next-line no-shadow
   enum NetworkDisclosureState {
-    Unspecified = 'Unspecified',
-    NoNetworkUsage = 'Does not contact external networks',
-    YesNetworkUsage = 'Contacts external networks'
+    Unspecified,
+    NoNetworkUsage,
+    YesNetworkUsage
   }
 
-  const networkDisclosureStateDescriptionKeys: Record<NetworkDisclosureState, string> = {
-    Unspecified: 'mod.network_disclosure.state.unspecified.description',
-    'Does not contact external networks': 'mod.network_disclosure.state.no_network_usage.description',
-    'Contacts external networks': 'mod.network_disclosure.state.yes_network_usage.description'
+  const optionTranslationKeys: Record<NetworkDisclosureState, string> = {
+    [NetworkDisclosureState.Unspecified]: 'mod.network_disclosure.state.unspecified.option_name',
+    [NetworkDisclosureState.NoNetworkUsage]: 'mod.network_disclosure.state.no_network_usage.option_name',
+    [NetworkDisclosureState.YesNetworkUsage]: 'mod.network_disclosure.state.yes_network_usage.option_name'
+  };
+
+  const descriptionTranslationKeys: Record<NetworkDisclosureState, string> = {
+    [NetworkDisclosureState.Unspecified]: 'mod.network_disclosure.state.unspecified.description',
+    [NetworkDisclosureState.NoNetworkUsage]: 'mod.network_disclosure.state.no_network_usage.description',
+    [NetworkDisclosureState.YesNetworkUsage]: 'mod.network_disclosure.state.yes_network_usage.description'
   };
 
   export let disclosure: string | null = null;
 
   let disclosureState = NetworkDisclosureState.Unspecified;
+  // Determine disclosure state from the existing disclosure value
   disclosureState =
     disclosure === null
       ? NetworkDisclosureState.Unspecified
@@ -50,20 +59,20 @@
   <span class="underline decoration-dotted" title={$t('mod.network_disclosure.header.tooltip')}
     ><T keyName="mod.network_disclosure.header" /></span>
   <select class="select" style="margin-bottom: 10px" bind:value={disclosureState} on:change={onStatePickerChange}>
-    {#each Object.values(NetworkDisclosureState) as state}
-      <option value={state}>{state}</option>
+    {#each Object.values(NetworkDisclosureState).filter((v) => typeof v === 'number') as state}
+      <option value={state}><T keyName={optionTranslationKeys[state]} /></option>
     {/each}
   </select>
   <p
     class="compatibility-state-description pb-4 {disclosureState === NetworkDisclosureState.Unspecified
       ? 'text-warning-500'
       : ''}">
-    <T keyName={networkDisclosureStateDescriptionKeys[disclosureState]} />
+    <T keyName={descriptionTranslationKeys[disclosureState]} />
   </p>
 </label>
 {#if disclosureState == NetworkDisclosureState.YesNetworkUsage}
   <label class="label">
-    <span><T keyName="mod.network_disclosure.developer.description.header" /></span>
+    <span><T keyName="mod.network_disclosure.developer.description.header" /> *</span>
     <textarea
       class="textarea p-4"
       bind:value={disclosure}
