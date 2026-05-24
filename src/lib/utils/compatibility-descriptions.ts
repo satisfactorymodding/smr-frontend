60bf403b286de76b7328705b910b41334cb60fe2
Rob B <computerguy440+gh@gmail.com>
2026-05-24 15:54:11 -0400

    refactor: use union type

--- src/lib/utils/compatibility-descriptions.ts
+++ src/lib/utils/compatibility-descriptions.ts
@@ -1,10 +1,7 @@
-import type { ControllerCompatibilityState } from '$lib/generated';
+import type { CompatibilityState, ControllerCompatibilityState } from '$lib/generated';
 import type { TolgeeInstance } from '@tolgee/svelte';
 
-export const compatibilityStateDescriptions: {
-  // TODO switch to Record, but Unknown isn't a valid db type
-  [key: string]: string;
-} = {
+export const compatibilityStateDescriptions: Record<CompatibilityState | 'Unknown', string> = {
   Works: 'compatibility-info.state.works.description',
   Damaged: 'compatibility-info.state.damaged.description',
   Broken: 'compatibility-info.state.broken.description',
@@ -19,7 +16,7 @@ export const controllerCompatibilityStateDescriptions: Record<ControllerCompatib
   Supported: 'controller-compatibility-info.state.supported.description'
 };
 
-export const getCompatibilityStateDescriptionFor = (state: string, tolgee: TolgeeInstance): string =>
+export const getCompatibilityStateDescriptionFor = (state: CompatibilityState | 'Unknown', tolgee: TolgeeInstance): string =>
   tolgee.t(compatibilityStateDescriptions[state]);
 
 export const getControllerCompatibilityStateDescriptionFor = (
