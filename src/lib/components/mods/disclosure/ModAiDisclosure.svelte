4a9477aff6eeb6e77db1a904ebb7c67b37bf7755
Rob B <computerguy440+gh@gmail.com>
2026-05-24 20:38:32 -0400

    feat: icons for disclosure types,  frame on disclosure body text

--- src/lib/components/mods/disclosure/ModAiDisclosure.svelte
+++ src/lib/components/mods/disclosure/ModAiDisclosure.svelte
@@ -7,37 +7,60 @@
 
   export let mod!: Pick<Mod, 'ai_use_disclosure'>;
 
+  const getIcon = (disclosure?: AiUseDisclosureType) => {
+    if (disclosure === undefined || disclosure === AiUseDisclosureType.NoDisclosure) {
+      return 'question_mark';
+    } else {
+      return {
+        [AiUseDisclosureType.NoAiUsage]: 'person',
+        [AiUseDisclosureType.AiUsage]: 'auto_awesome',
+        [AiUseDisclosureType.RuntimeAiUsage]: 'assistant'
+      }[disclosure];
+    }
+  };
+
+  $: hasDescription =
+    mod.ai_use_disclosure?.disclosure_type == AiUseDisclosureType.AiUsage ||
+    mod.ai_use_disclosure?.disclosure_type == AiUseDisclosureType.RuntimeAiUsage;
+
   export const { t } = getTranslate();
 </script>
 
 <div class="card p-4">
   <section>
     <div class="break-words text-lg">
-      <h3 class="my-4 text-2xl font-bold underline decoration-dotted" title={$t('mod.ai_disclosure.header.tooltip')}>
-        <T keyName="mod.ai_disclosure.header" />
+      <h3 class="my-4 flex items-center justify-between text-2xl">
+        <span class="truncate font-bold underline decoration-dotted" title={$t('mod.ai_disclosure.header.tooltip')}>
+          <T keyName="mod.ai_disclosure.header" />
+        </span>
+        <span
+          title={$t('symbol_for_status.tooltip')}
+          class="material-icons variant-filled-surface badge-icon p-4 text-2xl">
+          {getIcon(mod?.ai_use_disclosure?.disclosure_type)}
+        </span>
       </h3>
       {#if mod?.ai_use_disclosure === null || mod?.ai_use_disclosure?.disclosure_type === AiUseDisclosureType.NoDisclosure}
         <span>{$t('mod.ai_disclosure.no_ai_use_disclosure.description.user')}</span>
-        <br />
       {:else if mod?.ai_use_disclosure?.disclosure_type === AiUseDisclosureType.NoAiUsage}
         <span class="italic">{$t('mod.ai_disclosure.no_ai_use.description.user')}</span>
-        <br />
       {:else if mod?.ai_use_disclosure?.disclosure_type === AiUseDisclosureType.AiUsage}
         <span class="italic">{$t('mod.ai_disclosure.ai_use.description.user')}</span>
-        {#await markdown(mod?.ai_use_disclosure?.disclosure_string ?? 'Invalid State!') then rendered}
-          <!-- eslint-disable-next-line -->
-          {@html rendered}
-        {/await}<br />
       {:else if mod?.ai_use_disclosure?.disclosure_type === AiUseDisclosureType.RuntimeAiUsage}
         <span class="italic">{$t('mod.ai_disclosure.runtime_ai_use.description.user')} </span>
-        {#await markdown(mod?.ai_use_disclosure?.disclosure_string ?? 'Invalid State!') then rendered}
-          <!-- eslint-disable-next-line -->
-          {@html rendered}
-        {/await}<br />
       {/if}
+      {#if hasDescription}
+        <div class="card p-2">
+          {#await markdown(mod?.ai_use_disclosure?.disclosure_string ?? 'Invalid State!') then rendered}
+            <!-- eslint-disable-next-line -->
+            {@html rendered}
+          {/await}
+        </div>
+      {/if}
+      <br />
       <button
-        class="variant-ringed-surface variant-glass-surface btn btn-md m-6"
+        class="variant-ringed-surface variant-glass-surface btn btn-md"
         on:click={() => goto(base + '/content-policy')}>
+        <span class="material-icons">verified_user</span>
         <span>{$t('content-policy')}</span>
       </button>
     </div>
