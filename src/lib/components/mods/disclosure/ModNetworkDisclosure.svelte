b61a05f26149b46b6b66b6a3306da0c93d383581
Rob B <computerguy440+gh@gmail.com>
2026-05-24 20:38:32 -0400

    feat: icons for disclosure types,  frame on disclosure body text

--- src/lib/components/mods/disclosure/ModNetworkDisclosure.svelte
+++ src/lib/components/mods/disclosure/ModNetworkDisclosure.svelte
@@ -6,24 +6,40 @@
 
   export let mod!: Pick<Mod, 'network_use_disclosure'>;
 
+  const getIcon = (disclosure?: string | null) => {
+    if (disclosure === valueForNoUsageDisclosure) {
+      return 'sensors_off';
+    } else if (disclosure === valueForUnspecifiedDisclosure) {
+      return 'question_mark';
+    }
+    return 'sensors';
+  };
+
   export const { t } = getTranslate();
 </script>
 
 <div class="card p-4">
   <section>
     <div class="break-words text-lg">
-      <h3
-        class="my-4 text-2xl font-bold underline decoration-dotted"
-        title={$t('mod.network_disclosure.header.tooltip')}>
-        <T keyName="mod.network_disclosure.header" />
+      <h3 class="my-4 flex items-center justify-between text-2xl">
+        <span
+          class="truncate font-bold underline decoration-dotted"
+          title={$t('mod.network_disclosure.header.tooltip')}>
+          <T keyName="mod.network_disclosure.header" />
+        </span>
+        <span
+          title={$t('symbol_for_status.tooltip')}
+          class="material-icons variant-filled-surface badge-icon p-4 text-2xl">
+          {getIcon(mod?.network_use_disclosure)}
+        </span>
       </h3>
-      {#if mod?.network_use_disclosure === valueForUnspecifiedDisclosure}
+      {#if mod?.network_use_disclosure === valueForUnspecifiedDisclosure || mod?.network_use_disclosure == undefined}
         <p><T keyName="mod.network_disclosure.legacy_none_specified" /></p>
       {:else if mod.network_use_disclosure === valueForNoUsageDisclosure}
         <p class="italic"><T keyName="mod.network_disclosure.no_network_usage" /></p>
       {:else}
         <p class="italic"><T keyName="mod.network_disclosure.intro_message" /></p>
-        <div>
+        <div class="card p-2">
           {#await markdown(mod.network_use_disclosure) then rendered}
             <!-- eslint-disable-next-line -->
             {@html rendered}
