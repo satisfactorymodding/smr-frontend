0fd1268ebc3ba7a58dcfc64e613677aaa7651d6e
Rob B <computerguy440+gh@gmail.com>
2026-05-24 22:31:29 -0400

    fix: show "try it" message for untested controller compatibility

--- src/lib/components/mods/compatibility/CompatibilityGrid.svelte
+++ src/lib/components/mods/compatibility/CompatibilityGrid.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
+  import { ControllerCompatibilityState } from '$lib/generated';
   import type { CompatibilityInfoInput } from '$lib/generated';
   import CompatibilityStateText from '$lib/components/mods/compatibility/branch/CompatibilityStateText.svelte';
   import { markdown } from '../../../utils/markdown';
   import CompatibilityIcon from '$lib/components/mods/compatibility/branch/CompatibilityIcon.svelte';
   import { getModalStore } from '@skeletonlabs/skeleton';
   import CompatibilityModal from '$lib/modals/CompatibilityModal.svelte';
-  import { getTranslate } from '@tolgee/svelte';
+  import { getTranslate, T } from '@tolgee/svelte';
   import ControllerCompatibilityIcon from './controller/ControllerCompatibilityIcon.svelte';
   import ControllerCompatibilityStateText from './controller/ControllerCompatibilityStateText.svelte';
 
@@ -30,19 +31,21 @@
 
 <div class="card p-4">
   <div class="grid grid-flow-row gap-y-2">
-    <h3 class="my-4 text-2xl font-bold">{$t('compatibility-info.header')}</h3>
-    <p>{$t('compatibility-info.subtitle')}</p>
+    <h3 class="my-4 text-2xl font-bold"><T keyName="compatibility-info.header" /></h3>
+    <p><T keyName="compatibility-info.subtitle" /></p>
     <div class="grid grid-flow-row">
       <table aria-label="Branch Compatibility Information" class="max-w-auto table table-hover !overflow-visible">
         <tbody>
           <tr class="rounded border !border-surface-500">
             <td
               ><div class="flex items-center justify-center">
-                <CompatibilityIcon />{$t('compatibility-info.branch.stable')}
+                <CompatibilityIcon />
+                <T keyName="compatibility-info.branch.stable" />
               </div></td>
             <td
               ><div class="flex items-center justify-center">
-                <CompatibilityIcon EXP={true} />{$t('compatibility-info.branch.unstable')}
+                <CompatibilityIcon EXP={true} />
+                <T keyName="compatibility-info.branch.unstable" />
               </div></td>
           </tr>
           <tr class="rounded border !border-surface-500">
@@ -84,7 +87,7 @@
             {:else}
               <td colspan="2">
                 <span style="padding: 2px; white-space:normal">
-                  {$t('compatibility-info.state.unknown.description')}
+                  <T keyName="compatibility-info.state.unknown.description" />
                 </span>
               </td>
             {/if}
@@ -116,6 +119,8 @@
                   {#await markdown(compatibility.Controller.note) then rendered}
                     {@html rendered}
                   {/await}
+                {:else if compatibility?.Controller == null || compatibility.Controller.state === ControllerCompatibilityState.Untested}
+                  <T keyName="controller-compatibility-info.state.untested.description" />
                 {:else}
                   {noNotesText}
                 {/if}
